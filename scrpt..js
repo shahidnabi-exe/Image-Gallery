a2569fe3d142c4422ecc1b8e34089c47f10aecc2
Shahid Nabi <128903863+shahidnabi-exe@users.noreply.github.com>
2024-08-24 18:33:25 +0500

    fifth commit

--- scrpt..js
+++ scrpt..js
@@ -1,12 +1,39 @@
-
 let fullImgBox = document.getElementById("fullImgBox");
-let fullImg    = document.getElementById("fullImg");
+let fullImg = document.getElementById("fullImg");
+let currentIndex = 0;
 
-function openFullImg(pic) {
+const images = [
+    "Images/courtois.jpeg",
+    "Images/ramos.jpeg",
+    "Images/vvd.jpeg",
+    "Images/marcelo.jpeg",
+    "Images/kdb.jpeg",
+    "Images/Zizou.jpeg",
+    "Images/Modric.jpeg",
+    "Images/ozil.jpeg",
+    "Images/messi.jpeg",
+    "Images/CR7.jpeg",
+    "Images/neymar.jpeg"
+];
+
+function openFullImg(pic, index) {
+    currentIndex = index;
     fullImgBox.style.display = "flex";
     fullImg.src = pic;
 }
 
 function closeFullImg() {
-    fullImgBox.style.display ="none";
+    fullImgBox.style.display = "none";
+}
+
+function changeImage(direction) {
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    fullImg.src = images[currentIndex];
 }
